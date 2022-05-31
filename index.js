6021a568b07a885c730242cef7799d27e5ae9a27
ISB-YT <mcpebuildteam@gmail.com>
2022-05-30 20:59:48 -0400

    init

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express")
+const fetch = require("node-fetch")
+const app = express()
+
+require('dotenv').config()
+
+const cookie = process.env.COOKIE
+const userId = process.env.USER
+
+var recentAffiliateSales = []
+
+var isAffiliateSaleCheckDown = false
+
+app.get("/isSystemDown", async (req, res) => {
+    return res.json({
+        down: isAffiliateSaleCheckDown
+    })
+})
+
+app.get("/recentAffiliateSales/:assetId", async (req, res) => {
+    return res.json(recentAffiliateSales.filter(sale => {
+        return sale.details.id == req.params.assetId || req.params.assetId == undefined
+    }))
+})
+
+app.get("/recentAffiliateSales", async (req, res) => {
+    return res.json(recentAffiliateSales)
+})
+
+setInterval(() => {
+    fetch(`https://economy.roblox.com/v2/users/${userId}/transactions?transactionType=AffiliateSale&limit=10`, {
+        headers: {
+            cookie: `.ROBLOSECURITY=${cookie}`
+        }
+    })
+    .then(result => {
+        if(result.status == 200){
+            isAffiliateSaleCheckDown = false
+            return result.json()
+        }else{
+            throw new Error("Request failed.")
+        }
+    })
+    .then(json => {
+        if(json.data){
+            const data = json.data
+
+            data.forEach(element => {
+                if(recentAffiliateSales.find(ele => ele.id) == undefined){
+                    recentAffiliateSales.push(element)
+                }
+                recentAffiliateSales = recentAffiliateSales.splice(recentAffiliateSales.length - 1000)
+            })
+        }
+    })
+    .catch((e) => {
+        console.log(e)
+        isAffiliateSaleCheckDown = true
+    })
+}, 200);
+
+app.listen(process.env.PORT || 3030)
